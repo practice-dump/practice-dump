fc9c7ff4ebbf10a52be7264b74da6415f817683a
practice-dump <110192695+practice-dump@users.noreply.github.com>
2023-02-17 03:18:10 +0530

    Create generator_on_dataset.py

---
+++ Transformers/generator_on_dataset.py
@@ -0,0 +1,10 @@
+# KeyDataset is a util that will just output the item we're interested in.
+from transformers import pipeline
+from transformers.pipelines.pt_utils import KeyDataset
+from datasets import load_dataset
+
+pipe = pipeline(model="hf-internal-testing/tiny-random-wav2vec2", device=0)
+dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation[:10]")
+
+for out in pipe(KeyDataset(dataset, "audio")):
+    print(out)
