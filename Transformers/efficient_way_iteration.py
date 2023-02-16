d67ce3c83df1bdd39d40216a1fc1fb2070faad0a
practice-dump <110192695+practice-dump@users.noreply.github.com>
2023-02-17 03:32:11 +0530

    Update and rename generator_on_dataset.py to efficient_way_iteration.py

--- Transformers/efficient_way_iteration.py
+++ Transformers/efficient_way_iteration.py
@@ -1,10 +1,24 @@
-# KeyDataset is a util that will just output the item we're interested in.
 from transformers import pipeline
+## We are creating a generator
+
+def data():
+    for i in range(1000):
+        yield f"My example {i}"
+
+
+pipe = pipeline(model="gpt2", device=0)
+generated_characters = 0
+for out in pipe(data()):
+    generated_characters += len(out[0]["generated_text"])
+
+# KeyDataset is a util that will just output the item we're interested in.
+
 from transformers.pipelines.pt_utils import KeyDataset
 from datasets import load_dataset
 
 pipe = pipeline(model="hf-internal-testing/tiny-random-wav2vec2", device=0)
 dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation[:10]")
+## Directly iterating over dataset
 
 for out in pipe(KeyDataset(dataset, "audio")):
     print(out)
