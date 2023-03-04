a49eaa82b6289c32141eefa5f9d003511e955fbe
practice-dump <110192695+practice-dump@users.noreply.github.com>
2023-03-04 17:14:22 +0530

    Update different_components_of_transformers.py

--- Transformers/different_components_of_transformers.py
+++ Transformers/different_components_of_transformers.py
@@ -1,4 +1,5 @@
-###Loading tokenizer using Autotokenizer module
+#Loading tokenizer using Autotokenizer module
+## Converts text to integers
 from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -7,7 +8,22 @@ sequence = "In a hole in the ground there lived a hobbit."
 print(tokenizer(sequence))
 
 ##It will return three components
-#{'input_ids': [101, 1999, 1037, 4920, 1999, 1996, 2598, 2045, 2973, 1037, 7570, 10322, 4183, 1012, 102], 
-# 'token_type_ids': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-# 'attention_mask': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+#{'input_ids': [101, 1999, 1037, 4920, 1999, 1996, 2598, 2045, 2973, 1037, 7570, 10322, 4183, 1012, 102], ## This contains id to remap tokens
+##from ids to string
+# 'token_type_ids': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ## Signfies whether sentence is A or B (remember BERT pretraing)
+# 'attention_mask': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]} ## Signifies whether the token is here or this just padding
+
+#Loading ImageProcesser using AutoImageProcessor module
+## For vision tasks, an image processor processes the image into the correct input format.
+
+from transformers import AutoImageProcessor
+
+image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+
+# Loading feature extractor using AutoFeatureExtractor module
+##For audio tasks, a feature extractor processes the audio signal the correct input format.
+from transformers import AutoFeatureExtractor
+
+feature_extractor = AutoFeatureExtractor.from_pretrained(
+    "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition")
 
