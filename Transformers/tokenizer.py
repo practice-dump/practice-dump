d0ce7b1000e10c09a6154799753cb9aa308eca2f
practice-dump <110192695+practice-dump@users.noreply.github.com>
2023-03-04 18:04:40 +0530

    Create tokenizer.py

---
+++ Transformers/tokenizer.py
@@ -0,0 +1,27 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+batch_sentences = [
+    "But what about second breakfast?",
+    "Don't think he knows about second breakfast, Pip.",
+    "What about elevensies?",
+]
+encoded_input = tokenizer(batch_sentences, padding=True, truncation=True, return_tensors="pt")
+## Padding makes length of all text equal by adding special token at the end of all sentences 
+## Truncation = True, truncates the length of tokens to the maximum a model can taken
+## return_tensors="pt" : returns tensors 
+## One uses return_tensors="tf" in case they are using tensorflow
+print(encoded_input)
+
+'''
+{'input_ids': tensor([[101, 1252, 1184, 1164, 1248, 6462, 136, 102, 0, 0, 0, 0, 0, 0, 0],
+                      [101, 1790, 112, 189, 1341, 1119, 3520, 1164, 1248, 6462, 117, 21902, 1643, 119, 102],
+                      [101, 1327, 1164, 5450, 23434, 136, 102, 0, 0, 0, 0, 0, 0, 0, 0]]), 
+ 'token_type_ids': tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]), 
+ 'attention_mask': tensor([[1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                           [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]])}
+'''
