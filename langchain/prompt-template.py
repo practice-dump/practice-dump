11ba23f4ab9cac8e1a43ae84a28f55248d406fc0
practice-dump <110192695+practice-dump@users.noreply.github.com>
2025-10-04 17:01:38 +0530

    Update prompt-template.py

--- langchain/prompt-template.py
+++ langchain/prompt-template.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 model = ChatOpenAI()
 
-####### creating the prompt template in file
+####### creating the prompt template 
 # detailed way
 template2 = PromptTemplate(
     template='Greet this person in 5 languages. The name of the person is {name}',
@@ -24,4 +24,16 @@ print(result.content)
 template = load_prompt('template.json')
 
 
+
+####### creating the Chat prompt template 
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate([
+    ('system', 'You are a helpful {domain} expert'),
+    ('human', 'Explain in simple terms, what is {topic}')
+])
+
+prompt = chat_template.invoke({'domain':'cricket','topic':'Doosra'})
+
+print(prompt)
 
